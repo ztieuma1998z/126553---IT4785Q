8efcb3b4ee010b743bf1a4c1da09b1fceafe9076
ztieuma1998z <longdinhngo98@gmail.com>
2022-01-12 19:37:06 +0700

    jsondata

---
+++ JSONDATA/app/src/main/java/com/example/getdatajson/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.getdatajson;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        new DownloadTask(this).execute("https://lebavui.github.io/jsons/users.json");
+    }
+
+    class DownloadTask extends AsyncTask<String, Void, JSONArray> {
+        ProgressDialog dialog;
+        Context context;
+
+        public DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(context);
+            dialog.setMessage("Loading data...");
+            dialog.show();
+        }
+
+        @Override
+        protected JSONArray doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                String line;
+                StringBuilder builder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = reader.readLine()) != null)
+                    builder.append(line);
+                reader.close();
+
+                String jsonString = builder.toString();
+                Log.v("TAG", jsonString);
+
+                return new JSONArray(jsonString);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            dialog.dismiss();
+
+
+            if (jsonArray != null) {
+                JsonAdapter adapter = new JsonAdapter(context, jsonArray);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+    }
+}
+
